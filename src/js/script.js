8352fe6212ecc224362f02b8c5281d7af4e3eb05
Alexey Podobed <alexeyandmail@gmail.com>
2021-11-26 12:19:43 +0200

    added anim

--- src/js/script.js
+++ src/js/script.js
@@ -16,6 +16,35 @@ function ready() {
         observer.observe(elm);
     }
 
+    let windowinnerHeight = $(window).innerHeight();
+    let pageBottom = $(this).scrollTop() + windowinnerHeight - 100;
+
+    function animations() {
+
+        $('.banner-animation-hidden').each(function () {
+            showAnim($(this), 'banner-animation', 'banner-animation-hidden');
+        });
+
+        $('.banner-content-animation-hidden').each(function () {
+            showAnim($(this), 'banner-content-animation', 'banner-content-animation-hidden');
+        });
+
+        function showAnim($this, firstClass, secondClass) {
+            var currentOffsetTop = $this.offset().top;
+            if (currentOffsetTop < pageBottom) {
+                $this.addClass(firstClass).removeClass(secondClass);
+            }
+        }
+    }
+
+    animations();
+
+    $(window).scroll(function () {
+        windowinnerHeight = $(window).innerHeight() + 100;
+        pageBottom = $(this).scrollTop() + windowinnerHeight;
+        animations();
+    });
+
 }
 
 document.addEventListener("DOMContentLoaded", ready);
